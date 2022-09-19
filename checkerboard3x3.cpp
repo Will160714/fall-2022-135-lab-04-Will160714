2ae2ff5a5bc8c4f6265ef574374191d85cc74f6e
Will160714 <patrick@LAPTOP-I8OEL6NV.localdomain>
2022-09-18 23:16:31 -0400

    In Progress

---
+++ checkerboard3x3.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include "all.h"
+
+std::string check3x3(int width, int height){
+    std::string result = "";
+    result += "Input Width: " + std::to_string(width) + "\n";
+    result += "Input Height: " + std::to_string(height) + "\n\n";
+    result += "Shape: \n";
+
+    bool star = true;
+    int count = 0;
+    int change = 0;
+    std::string stars = "*";
+    std::string space = " ";
+    for(int x = 0; x < height; x ++){
+        for(int y = 0; y < width; y++){
+            if(star)
+                result += stars;
+            else
+                result += space;
+
+            count ++;
+
+            if(count == 3){
+                count = 0;
+                if(star)
+                    star = false;
+                else
+                    star = true;
+            }
+        }
+
+        result += "\n";
+        if(star)
+            star = false;
+        else
+            star = true;
+
+        if(x % 3 == 0){
+            change ++;
+        }
+
+        if(change % 2 != 0){
+            stars = " ";
+            space = " ";
+        }
+        else{
+            stars = "*";
+            space = " ";
+        }
+    }
+
+    return result;
+}
